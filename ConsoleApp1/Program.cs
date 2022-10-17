ad1f6506aeb28b8ad897004fe75321ed2458098f
Ovsyanka666 <valps319@mail.ru>
2022-10-17 20:05:00 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static IEnumerable<string> GetAllStudents(Classroom[] classes)
+        {
+            var result = from classrom in classes
+                         from s in classrom.Students
+                         select s;
+
+            return result;
+        }        
+    }
+}
